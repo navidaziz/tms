46d48214bd87dea61041fea107b6bc30b2e37338
navidaziz98 <navidaziz98@gmail.com>
2023-12-26 22:23:48 +0500

    add attendance and test result module for management

--- application/views/admin/trainings/training_batch_detail.php
+++ application/views/admin/trainings/training_batch_detail.php
@@ -53,12 +53,20 @@ $currentDate = clone $startDate; ?>
 
         <div class="box border blue" id="messenger">
             <div class="box-title">
-                <h4><i class="fa fa-calendar"></i>Day Wise Sessions and Schedule</h4>
+                <h4><i class="fa fa-calendar"></i>Training Batch Detail</h4>
 
             </div>
             <div class="box-body">
                 <div class="tabbable header-tabs">
                     <ul class="nav nav-tabs">
+                        <li <?php if ($this->input->get('tab') == 'test_result') { ?>class="active" <?php } ?>>
+                            <a href="#box_tab4" data-toggle="tab"><i class="fa fa-check" aria-hidden="true"></i>
+                                <span class="hidden-inline-mobile">Pre / Post Test Results</span></a>
+                        </li>
+                        <li <?php if ($this->input->get('tab') == 'attandance') { ?>class="active" <?php } ?>>
+                            <a href="#box_tab3" data-toggle="tab"><i class="fa fa-clock-o" aria-hidden="true"></i>
+                                <span class="hidden-inline-mobile">Attendance</span></a>
+                        </li>
                         <li <?php if ($this->input->get('tab') == 'trainees') { ?>class="active" <?php } ?>>
                             <a href="#box_tab2" data-toggle="tab">
                                 <i class="fa fa-users"></i>
@@ -68,6 +76,7 @@ $currentDate = clone $startDate; ?>
                             <a href="#box_tab1" data-toggle="tab"><i class="fa fa-calendar" aria-hidden="true"></i>
                                 <span class="hidden-inline-mobile">Day Wise Sessions and Schedule</span></a>
                         </li>
+
                     </ul>
                     <div class="tab-content">
                         <div class="tab-pane fade in <?php if ($this->input->get('tab') == 'session') { ?> in active <?php } ?>" id="box_tab1">
@@ -200,10 +209,10 @@ $currentDate = clone $startDate; ?>
                                         <tbody>
                                             <?php
                                             $query = "SELECT users.*, training_nominations.nomination_type,  training_nominations.id
-        FROM training_nominations 
-        INNER JOIN users ON(users.user_id = training_nominations.user_id)
-        WHERE training_nominations.batch_id = " . $batch->batch_id . "
-        AND training_nominations.training_id = " . $training->training_id;
+                                            FROM training_nominations 
+                                            INNER JOIN users ON(users.user_id = training_nominations.user_id)
+                                            WHERE training_nominations.batch_id = " . $batch->batch_id . "
+                                            AND training_nominations.training_id = " . $training->training_id;
                                             $nominations = $this->db->query($query)->result();
                                             $count = 1;
                                             foreach ($nominations as $nomination) : ?>
@@ -227,6 +236,231 @@ $currentDate = clone $startDate; ?>
                                     <div style="text-align: center;">
                                         <button onclick="get_nomination_list('Trainee')" class="btn btn-primary btn-sm" style="padding: 2px 10px; font-size:11px;">Add Trainees</button>
                                     </div>
+                                </div>
+                            </div>
+
+                            <hr class="margin-bottom-0">
+                            <!-- /TAB 1 -->
+                        </div>
+
+
+                        <div class="tab-pane fade in <?php if ($this->input->get('tab') == 'attendance') { ?> in active <?php } ?>" id="box_tab3">
+                            <div class="col-md-12">
+                                <div class="table-responsive">
+
+                                    <table class="table table-bordered" style="font-size: 9px;">
+                                        <thead>
+                                            <tr>
+                                                <th colspan="8"></th>
+                                                <?php
+                                                $startDate = new DateTime($batch->batch_start_date);
+                                                $endDate = new DateTime($batch->batch_end_date);
+                                                $currentDate = clone $startDate;
+                                                while ($currentDate <= $endDate) {
+                                                    $currentDate->modify('+1 day');
+                                                ?>
+                                                    <th colspan="2" style="text-align: center;">
+                                                        <strong><?php echo date('D d M y', strtotime($currentDate->format('Y-m-d'))); ?></strong>
+                                                    </th>
+
+                                                <?php } ?>
+
+                                            </tr>
+                                            <tr>
+                                                <th>S.No</th>
+                                                <th>Name</th>
+                                                <th>Father Name</th>
+                                                <th>CNIC</th>
+                                                <th>Designation</th>
+                                                <th>District</th>
+                                                <th>Qualification</th>
+                                                <th>Mobile No.</th>
+                                                <?php
+                                                $startDate = new DateTime($batch->batch_start_date);
+                                                $endDate = new DateTime($batch->batch_end_date);
+                                                $currentDate = clone $startDate;
+                                                while ($currentDate <= $endDate) {
+                                                    $currentDate->modify('+1 day');
+                                                ?>
+                                                    <th style="text-align: center;"> <i class="fa fa-sun-o" aria-hidden="true"></i> M</th>
+                                                    <th style="text-align: center;"> <i class="fa fa-moon-o" aria-hidden="true"></i> E</th>
+                                                <?php } ?>
+
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $query = "SELECT users.*, training_nominations.nomination_type,  training_nominations.id
+                                            FROM training_nominations 
+                                            INNER JOIN users ON(users.user_id = training_nominations.user_id)
+                                            WHERE training_nominations.batch_id = " . $batch->batch_id . "
+                                            AND training_nominations.training_id = " . $training->training_id;
+                                            $nominations = $this->db->query($query)->result();
+                                            $count = 1;
+                                            foreach ($nominations as $nomination) : ?>
+                                                <tr>
+                                                    <td><?php echo $count++; ?></td>
+                                                    <td><?php echo $nomination->name; ?></td>
+                                                    <td><?php echo $nomination->father_name; ?></td>
+                                                    <td><?php echo $nomination->cnic; ?></td>
+                                                    <td><?php echo $nomination->designation; ?></td>
+                                                    <td><?php echo $nomination->district; ?></td>
+                                                    <td><?php echo $nomination->qualification; ?></td>
+                                                    <td><?php echo $nomination->user_mobile_number; ?></td>
+                                                    <?php
+                                                    $startDate = new DateTime($batch->batch_start_date);
+                                                    $endDate = new DateTime($batch->batch_end_date);
+                                                    $currentDate = clone $startDate;
+                                                    while ($currentDate <= $endDate) {
+                                                        $currentDate->modify('+1 day');
+                                                        $attendance_date = $currentDate->format('Y-m-d');
+                                                        $query = "SELECT morning_time FROM `attendances`
+                                                                WHERE training_id = '" . $training->training_id . "'
+                                                                AND batch_id = '" . $batch->batch_id . "'
+                                                                AND user_id = '" . $nomination->user_id . "'
+                                                                AND attendance_date = '" . $attendance_date . "'
+                                                                AND morning_time IS NOT NULL";
+                                                        $morning_attendance = $this->db->query($query)->row();
+                                                        if ($morning_attendance) { ?>
+                                                            <th style="text-align: center;"><?php echo $morning_attendance->morning_time; ?> </th>
+                                                        <?php } else { ?>
+                                                            <th style="text-align: center;">A</th>
+                                                        <?php } ?>
+
+                                                        <?php $query = "SELECT evening_time FROM `attendances`
+                                                                WHERE training_id = '" . $training->training_id . "'
+                                                                AND batch_id = '" . $batch->batch_id . "'
+                                                                AND user_id = '" . $nomination->user_id . "'
+                                                                AND attendance_date = '" . $attendance_date . "'
+                                                                AND evening_time IS NOT NULL";
+                                                        $evening_time = $this->db->query($query)->row();
+                                                        if ($evening_time) { ?>
+                                                            <th style="text-align: center;"><?php echo $evening_time->morning_time; ?> </th>
+                                                        <?php } else { ?>
+                                                            <th style="text-align: center;">A</th>
+                                                        <?php } ?>
+
+
+
+                                                    <?php } ?>
+                                                </tr>
+
+
+                                            <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+
+
+                                </div>
+                            </div>
+
+                            <hr class="margin-bottom-0">
+                            <!-- /TAB 1 -->
+                        </div>
+
+                        <div class="tab-pane fade in <?php if ($this->input->get('tab') == 'test_result') { ?> in active <?php } ?>" id="box_tab4">
+                            <div class="col-md-12">
+                                <div class="table-responsive">
+
+                                    <table class="table table-bordered" style="font-size: 9px;">
+                                        <thead>
+                                            <tr>
+                                                <th colspan="8"></th>
+                                                <th colspan="4">Pre Test Result</th>
+                                                <th colspan="4">Post Test Result</th>
+
+                                            </tr>
+                                            <tr>
+                                                <th>S.No</th>
+                                                <th>Name</th>
+                                                <th>Father Name</th>
+                                                <th>CNIC</th>
+                                                <th>Designation</th>
+                                                <th>District</th>
+                                                <th>Qualification</th>
+                                                <th>Mobile No.</th>
+
+                                                <th>Total Question</th>
+                                                <th>Wrong Answers</th>
+                                                <th>Correct Answers</th>
+                                                <th>Percentage (%)</th>
+
+                                                <th>Total Question</th>
+                                                <th>Wrong Answers</th>
+                                                <th>Correct Answers</th>
+                                                <th>Percentage (%)</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $query = "SELECT users.*, training_nominations.nomination_type,  training_nominations.id
+                                            FROM training_nominations 
+                                            INNER JOIN users ON(users.user_id = training_nominations.user_id)
+                                            WHERE training_nominations.batch_id = " . $batch->batch_id . "
+                                            AND training_nominations.training_id = " . $training->training_id;
+                                            $nominations = $this->db->query($query)->result();
+                                            $count = 1;
+                                            foreach ($nominations as $nomination) : ?>
+                                                <tr>
+                                                    <td><?php echo $count++; ?></td>
+                                                    <td><?php echo $nomination->name; ?></td>
+                                                    <td><?php echo $nomination->father_name; ?></td>
+                                                    <td><?php echo $nomination->cnic; ?></td>
+                                                    <td><?php echo $nomination->designation; ?></td>
+                                                    <td><?php echo $nomination->district; ?></td>
+                                                    <td><?php echo $nomination->qualification; ?></td>
+                                                    <td><?php echo $nomination->user_mobile_number; ?></td>
+
+                                                    <?php
+                                                    $trainee_id = $this->session->userdata('userId');
+                                                    $query = "SELECT COUNT(*) as total, 
+                                                        SUM(IF(pre_test_result=1,1,0)) as correct_ans,
+                                                                    SUM(IF(pre_test_result=0,1,0)) as wrong_ans 
+                                                                    FROM training_tests
+                                                                        WHERE training_id = " . $training->training_id . "
+                                                                        AND batch_id = " . $batch->batch_id . "
+                                                                        AND trainee_id = " . $nomination->user_id . "";
+                                                    $summary = $this->db->query($query)->row();
+                                                    ?>
+
+                                                    <td><?php echo $summary->total; ?></td>
+                                                    <td><?php echo $summary->wrong_ans; ?></td>
+                                                    <td><?php echo $summary->correct_ans; ?></td>
+                                                    <td><?php
+                                                        if ($summary->total) {
+                                                            echo @round(($summary->correct_ans * 100) / $summary->total, 2) . " %";
+                                                        }
+                                                        ?></td>
+
+                                                    <?php
+                                                    $trainee_id = $this->session->userdata('userId');
+                                                    $query = "SELECT COUNT(*) as total, 
+                                                                SUM(IF(post_test_result=1,1,0)) as correct_ans,
+                                                                    SUM(IF(post_test_result=0,1,0)) as wrong_ans 
+                                                                    FROM training_tests
+                                                                WHERE training_id = " . $training->training_id . "
+                                                                        AND batch_id = " . $batch->batch_id . "
+                                                                        AND trainee_id = " . $nomination->user_id . "";
+                                                    $summary = $this->db->query($query)->row(); ?>
+                                                    <td><?php echo $summary->total; ?></td>
+                                                    <td><?php echo $summary->wrong_ans; ?></td>
+                                                    <td><?php echo $summary->correct_ans; ?></td>
+                                                    <td><?php
+                                                        if ($summary->total) {
+                                                            echo @round(($summary->correct_ans * 100) / $summary->total, 2) . " %";
+                                                        }
+                                                        ?></td>
+
+
+
+                                                </tr>
+
+
+                                            <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+
+
                                 </div>
                             </div>
 
